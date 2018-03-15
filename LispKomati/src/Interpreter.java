34530a089493354a17a3e5af714fc0eb46fa1b46
pavani1092 <pavani.1092@gmail.com>
2018-03-15 15:38:56 -0400

    Exception files

---
+++ LispKomati/src/Interpreter.java
@@ -0,0 +1,10 @@
+
+public class Interpreter {
+
+	public Interpreter() {
+		// TODO Auto-generated constructor stub
+	}
+	SExpr plus(SExpr a, SExpr b) {
+		return null;
+	}
+}
---
+++ LispKomati/src/MyException.java
@@ -0,0 +1,8 @@
+class MyException extends Exception
+{
+    public MyException(String s)
+    {
+        // Call constructor of parent Exception
+        super(s);
+    }
+}
